d7af56e50b106130882013653fcdbc6a7bab47dd
Erika Bernardo Lima <20242014040026@ifrn.local>
2025-01-16 15:43:58 -0300

    tentativa de resolução da questão 2 e 3 da lista 08

--- Codeforces/Lista08/02_meiaVida.py
+++ Codeforces/Lista08/02_meiaVida.py
@@ -1,10 +1,10 @@
 tempo, massa = map(int, input().split())
 
+
 soma = 0
 
 while (massa > 0.5):
     massa = massa / 2
-    # print(massa)
     soma = soma + 1
 
 soma = soma * tempo
@@ -14,16 +14,9 @@ min = 0
 h = 0
 dia = 0
 
-if soma < 60:
-    seg = soma
-    print("{} dias {:02d}:{:02d}:{:02d}".format(dia, h, min, seg))
-elif soma >= 60:
-    min = soma
-    seg = int(min % 60)
-    min = int(min / 60)
-    print("{} dias {:02d}:{:02d}:{:02d}".format(dia, h, min, seg))
-    if min > 60:
-        seg = int(min % 60)
-        min = int(min / 60)
-        h = int(min % 60)
-        print("{} dias {:02d}:{:02d}:{:02d}".format(dia, h, min, seg))
+dia = soma // 86400
+min = soma % 60
+
+print(dia)
+print(soma)
+print("{} dias {:02d}:{:02d}:{:02d}".format(dia, h, min, seg))
---
+++ Codeforces/Lista08/03_pulaSapo.py
@@ -0,0 +1,13 @@
+pulo, numCano = map(int, input().split())
+
+
+for i in range(numCano):
+    altCano = int(input())
+
+    cano = 0
+    cano = altCano
+
+if altCano < pulo:
+    print("GAME OVER")
+else:
+    print("YOU WIN")
