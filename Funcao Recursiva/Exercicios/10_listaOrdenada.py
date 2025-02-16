97d3e82e6711bea8bfc0824f5fb0376c9d4d6110
Erika Bernardo <erikabernardo949@gmail.com>
2025-02-16 20:55:16 -0300

    atualizações

--- Funcao Recursiva/Exercicios/10_listaOrdenada.py
+++ Funcao Recursiva/Exercicios/10_listaOrdenada.py
@@ -8,10 +8,10 @@ def lista_ordenada(l):
     if len(l) < 2:
         return True
     else:
-        if l[-1] > l[-2]:
-            return True
+        if l[0] <= l[1]:
+            return lista_ordenada(l[:-1]) 
         else:
             return False
         
 
-print(lista_ordenada([7,8,9,10]))
+print(lista_ordenada([7,9,9,10]))
